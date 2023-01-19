dc634ee951fa0022c35234301fce8049f5d45ae4
ItzSelenux <74371741+ItzSelenux@users.noreply.github.com>
2023-01-19 00:01:17 -0600

    first commit

---
+++ custom.py
@@ -0,0 +1,156 @@
+import subprocess
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import os
+
+
+
+class custom(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Type a resolution")
+
+        self.set_default_size(555, 444)
+
+        grid = Gtk.Grid()
+        grid.set_column_homogeneous(True)
+        grid.set_row_homogeneous(True)
+        self.add(grid)
+
+        label_resolution = Gtk.Label(label="Width:")
+        grid.attach(label_resolution, 0, 0, 1, 1)
+
+        self.Width = Gtk.Entry()
+        self.Width.set_placeholder_text("1920")
+        self.Width.set_text("1920")
+        self.Width.connect("insert-text", self.on_insert_text)
+        grid.attach(self.Width, 1, 0, 1, 1)
+
+        label_resolution = Gtk.Label(label="Height:")
+        grid.attach(label_resolution, 0, 1, 1, 1)
+
+        self.Height = Gtk.Entry()
+        self.Height.set_placeholder_text("1080")
+        self.Height.set_text("1080")
+        self.Height.connect("insert-text", self.on_insert_text)
+        grid.attach(self.Height, 1, 1, 1, 1)
+
+        Hz = Gtk.Label(label="Refresh Rate (Hz):")
+        grid.attach(Hz, 0, 2, 1, 1)
+
+        self.Hz = Gtk.Entry()
+        self.Hz.set_placeholder_text("60")
+        self.Hz.set_text("60")
+        self.Hz.connect("insert-text", self.on_insert_text)
+        grid.attach(self.Hz, 1, 2, 1, 1)
+
+        apply_button = Gtk.Button(label="Generate")
+        apply_button.connect("clicked", self.on_apply_clicked)
+        grid.attach(apply_button, 0, 3, 2, 1)
+
+
+        apply_text = Gtk.Label(label="WARNING: adding a custom resolution may cause unexpected results, \n like black screen if is unsopported by the Display or errors on resolutions list")
+        grid.attach(apply_text, 0, 4, 2, 1)
+    def on_insert_text(self, entry, new_text, new_text_length, position):
+        # only allow numeric input
+        if not new_text.isdigit():
+            entry.stop_emission_by_name("insert-text")
+        if len(entry.get_text()) > 4:
+            entry.set_text(entry.get_text()[:4])
+
+    def on_apply_clicked(self, button):
+        self.width = self.Width.get_text()
+        self.height = self.Height.get_text()
+        self.hz = self.Hz.get_text()
+        self.command = "cvt" + " " + self.width + " " + self.height + " " +  self.hz +" | grep Modeline | cut -c 10-"
+        self.result = subprocess.run(self.command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.default = "cvt 1300 600 65 | grep Modeline | cut -c 10- "
+        self.defaultshow = subprocess.run(self.default, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.getdisplay = "xrandr | grep -w " + " connected" + "| awk '{print $1}'"
+        self.display = subprocess.run(self.getdisplay, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.name = (self.width + "x" + self.height + "_" + self.hz + ".00")
+        
+
+
+        if not self.width or not self.height or not self.hz:
+            dialog = Gtk.MessageDialog(parent=self, modal=True, message_type=Gtk.MessageType.ERROR,
+    buttons=Gtk.ButtonsType.OK, message_format="Error")
+            dialog.format_secondary_text("Width, Height, and Hz are required fields, but this is an example of a custom resolution by 1300*600@65Hz: \n")
+    # Create a vertical box
+
+
+            grid = Gtk.Grid()
+            grid.set_column_homogeneous(True)
+            grid.set_row_homogeneous(True)
+            self.add(grid)
+
+            self.label1 = Gtk.Label(label="xrandr --newmode " + defaultshow.stdout.decode())
+            self.label1.set_selectable(True)
+            grid.attach(self.label1, 1, 0, 1, 1)
+
+            self.label2 = Gtk.Label(label="xrandr --addmode " + display.stdout.decode().strip() + " 65.00Hz")
+            self.label2.set_selectable(True)
+            grid.attach(self.label2, 1, 1, 1, 1)
+
+    # Add the box to the dialog
+            dialog.get_content_area().add(grid)
+            dialog.show_all()
+            dialog.run()
+            dialog.destroy()
+
+        else:
+            dialog = Gtk.MessageDialog(parent=self, modal=True, message_type=Gtk.MessageType.ERROR,
+    buttons=Gtk.ButtonsType.OK, message_format="Generated Resolution")
+            dialog.format_secondary_text("copy this commands and add it to your autostart, or execute this on a terminal to use only on this session, you need to restart SeleRandr to see new resolution \n")
+    # Create a vertical box
+
+
+            grid = Gtk.Grid()
+            grid.set_column_homogeneous(True)
+            grid.set_row_homogeneous(True)
+            self.add(grid)
+
+            self.label1 = Gtk.Label(label="xrandr --newmode " + self.result.stdout.decode())
+            self.label1.set_selectable(True)
+            grid.attach(self.label1, 1, 0, 1, 1)
+
+            self.label2 = Gtk.Label(label="xrandr --addmode " + self.display.stdout.decode().strip() + " " + self.name)
+            self.label2.set_selectable(True)
+            grid.attach(self.label2, 1, 1, 1, 1)
+
+            session_button = Gtk.Button(label="Add Resolution only on this session")
+            session_button.connect("clicked", self.on_session_clicked)
+            grid.attach(session_button, 1, 2, 1, 1)
+
+            print_button = Gtk.Button(label="Save commands to a file")
+            print_button.connect("clicked", self.on_print_clicked)
+            grid.attach(print_button, 1, 3, 1, 1)
+
+            self.label1 = Gtk.Label(label="File Will be saved on ~/NewResolutions, you also can see old saved resolutions")
+            grid.attach(self.label1, 1, 4, 1, 1)
+    # Add the box to the dialog
+            dialog.get_content_area().add(grid)
+            dialog.show_all()
+            dialog.run()
+            dialog.destroy()
+
+    def on_session_clicked(self, button):
+        cmd1 = "xrandr --newmode " + self.result.stdout.decode()        
+        subprocess.run(cmd1, shell=True)
+        cmd2 = "xrandr --addmode " + self.display.stdout.decode().strip() + " " + self.name        
+        subprocess.run(cmd2, shell=True)
+
+    def on_print_clicked(self, button):
+        home = os.path.expanduser("~" + "/")
+        cmd0 = "echo '" + self.name + "\n ----- ' >> " + home + "NewResolutions"
+        subprocess.run(cmd0, shell=True)
+        cmd1 = "echo " + "xrandr --newmode " + self.result.stdout.decode().strip() + ">>" + home + "NewResolutions"      
+        subprocess.run(cmd1, shell=True)
+        cmd2 = "echo '" + "xrandr --addmode " + self.display.stdout.decode().strip() + " " + self.name + "\n ----- \n' >> " + home + "NewResolutions"
+        subprocess.run(cmd2, shell=True)
+
+window = custom()
+window.connect("delete-event", Gtk.main_quit)
+window.show_all()
+Gtk.main()
+
---
+++ selerandr.py
@@ -0,0 +1,158 @@
+import subprocess
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk, GdkX11
+
+class selerandr(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="SeleRandr")
+
+        self.set_default_size(555, 444)
+
+        menubar = Gtk.MenuBar()
+        
+        # Close button
+        close_button = Gtk.Button()
+        close_button.set_image(Gtk.Image.new_from_icon_name("window-close", Gtk.IconSize.BUTTON))
+        close_button.connect("clicked", Gtk.main_quit)
+        
+        title_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        title_box.pack_start(menubar, True, True, 0)
+        title_box.pack_end(close_button, False, False, 0)
+        
+        self.set_titlebar(title_box)
+
+        #help option
+        help_menu = Gtk.MenuItem(label="Help")
+        help_menu.set_submenu(Gtk.Menu())
+        menubar.append(help_menu)
+        
+        # Create the help option item
+        help_item = Gtk.MenuItem(label="About")
+        help_item.connect("activate", self.on_about_clicked)
+        help_menu.get_submenu().append(help_item)
+
+        
+        self.show_all()
+
+        grid = Gtk.Grid()
+        grid.set_column_homogeneous(True)
+        grid.set_row_homogeneous(True)
+        self.add(grid)
+
+        label_resolution = Gtk.Label(label="Resolution:")
+        grid.attach(label_resolution, 0, 0, 1, 1)
+
+        self.resolution_combo = Gtk.ComboBoxText()
+        resolutions = self.get_resolutions()
+        for resolution in resolutions:
+            self.resolution_combo.append_text(resolution[0])
+        grid.attach(self.resolution_combo, 1, 0, 1, 1)
+
+        label_refresh_rate = Gtk.Label(label="Refresh Rate:")
+        grid.attach(label_refresh_rate, 0, 1, 1, 1)
+
+        self.refresh_rate_combo = Gtk.ComboBoxText()
+        refresh_rates = set([res[1] for res in resolutions])
+        for refresh_rate in refresh_rates:
+            self.refresh_rate_combo.append_text(refresh_rate)
+        grid.attach(self.refresh_rate_combo, 1, 1, 1, 1)
+
+        label_rotate = Gtk.Label(label="Rotate:")
+        grid.attach(label_rotate, 0, 2, 1, 1)
+
+        self.rotate_combo = Gtk.ComboBoxText()
+        self.rotate_combo.append_text("normal")
+        self.rotate_combo.append_text("left")
+        self.rotate_combo.append_text("right")
+        self.rotate_combo.append_text("inverted")
+        grid.attach(self.rotate_combo, 1, 2, 1, 1)
+
+        label_output = Gtk.Label(label="Output:")
+        grid.attach(label_output, 0, 3, 1, 1)
+
+        self.output_combo = Gtk.ComboBoxText()
+        outputs = self.get_outputs()
+        for output in outputs:
+            self.output_combo.append_text(output)
+        grid.attach(self.output_combo, 1, 3, 1, 1)
+
+        self.onoff_combo = Gtk.ComboBoxText()
+        self.onoff_combo.append_text("On")
+        self.onoff_combo.append_text("Off")
+        grid.attach(self.onoff_combo, 2, 3, 1, 1)
+
+        if len(outputs) == 1:
+            self.output_combo.set_sensitive(False)
+            self.onoff_combo.set_sensitive(False)
+
+        auto_button = Gtk.Button(label="Add Custom Resolution")
+        auto_button.connect("clicked", self.on_custom_clicked)
+        grid.attach(auto_button, 0, 4, 1, 1)
+
+        auto_button = Gtk.Button(label="Default")
+        auto_button.connect("clicked", self.on_auto_clicked)
+        grid.attach(auto_button, 1, 4, 1, 1)
+       
+        apply_button = Gtk.Button(label="Apply")
+        apply_button.connect("clicked", self.on_apply_clicked)
+        grid.attach(apply_button, 2, 4, 1, 1)
+
+
+        self.resolution_combo.set_active(0)
+        self.refresh_rate_combo.set_active(0)
+        self.rotate_combo.set_active(0)
+        self.output_combo.set_active(0)
+        self.onoff_combo.set_active(0)
+
+    def on_about_clicked(self, widget):
+        about_dialog = Gtk.AboutDialog()
+        about_dialog.set_program_name("SeleRandr")
+        about_dialog.set_version("1.0")
+        about_dialog.set_copyright("This program is licensed under MIT License")
+        about_dialog.set_comments("SeleRandr is a standalone GUI for Xrandr")
+        about_dialog.set_website("https://github.com/ItzSelenux/selerandr")
+        about_dialog.set_website_label("Source Code")
+        about_dialog.set_authors(["~ItzSelenux"])
+        icon_theme = Gtk.IconTheme.get_default()
+        icon = icon_theme.load_icon("video-display", 64, 0)
+        about_dialog.set_logo(icon)
+        about_dialog.run()
+        about_dialog.destroy()
+
+
+    def on_apply_clicked(self, button):
+        resolution, refresh_rate = self.resolution_combo.get_active_text(), self.refresh_rate_combo.get_active_text()
+        output = self.output_combo.get_active_text()
+        rotate = self.rotate_combo.get_active_text()
+        subprocess.run(["xrandr", "--output", output, "--mode", resolution, "--rate", refresh_rate, "--rotate", rotate])
+
+    def on_auto_clicked(self, button):
+        output = self.output_combo.get_active_text()
+        subprocess.run(["xrandr", "--output", output, "--auto", "--rotate", "normal"])
+
+    def on_custom_clicked(self, button):
+        subprocess.run(["python3", "custom.py"])
+
+    def get_resolutions(self):
+        xrandr_output = subprocess.run(["xrandr"], capture_output=True, text=True).stdout
+        resolutions = []
+        for line in xrandr_output.split("\n"):
+            if "  " in line:
+                resolution, refresh_rate = line.split()[0], line.split()[1]
+                resolutions.append((resolution, refresh_rate))
+        return resolutions
+
+    def get_outputs(self):
+        xrandr_output = subprocess.run(["xrandr"], capture_output=True, text=True).stdout
+        outputs = []
+        for line in xrandr_output.split("\n"):
+            if " connected" in line:
+                outputs.append(line.split()[0])
+        return outputs
+
+
+win = selerandr()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
