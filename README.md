39976c825b0e9d5f603ed9077db08093c7766164
ItzSelenux <ItzSelenux@zariep.com>
2023-03-02 11:07:05 -0600

    .

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # SeleRandr
 
-To DO
+## TO DO: 
 
-Dynamic Refresh Rate: pending for implementation
-Custom Resolution: not implemented
-scale: not implemented
-change position: not imlpemented
+- [x] Get a List of Resolutions and change the display resolution
+- [x] Get a List of Refresh Rates and change display refresh rate 
+- [x] Change Refresh Rate list dynamically 
+- [x] Button to change Rotation of display
+- [x] Slider to change scale of display
+- [x] Get a List of output and on/off selected output
+- [ ] Custom Resolution Creator
+- [ ] Change Position of Display if there is more than one display
+- [ ] Change the list of Resolutions dynamically
 
+## Build 
+´´
+ gcc `pkg-config --cflags gtk+-3.0` -o "%e"  "%f" `pkg-config --libs gtk+-3.0`
 
+´´
 standalone GUI for xrandr, with options to Resolutions, Refresh rate, rotate, scale. on/off output and add custom resolutions
 <img src="https://github.com/ItzSelenux/ItzSelenux.github.io/blob/main/res/pages/selerandr/1.png?raw=true">
 <img src="https://github.com/ItzSelenux/ItzSelenux.github.io/blob/main/res/pages/selerandr/2.png?raw=true">
--- selerandr.c
+++ selerandr.c
@@ -1,8 +1,82 @@
 #include <gtk/gtk.h>
 #include "selerandr.h"
  
+//Function to change rate when other resolution is selected
+static void on_rescombo_changed(GtkComboBox *combo_box, gpointer user_data) {
+    GtkComboBoxText *combo_text = GTK_COMBO_BOX_TEXT(combo_box);
+       const gchar *pres = gtk_combo_box_text_get_active_text(combo_text);
 
-int main(int argc, char *argv[])
+    char *sres = g_strdup_printf("%s ", pres);  // allocate enough space for the copied string and the space character
+
+    char **get_rates() {
+        char buffer[BUFFER_SIZE];
+        char **rates = malloc(BUFFER_SIZE * sizeof(char *));
+        int count = 0;
+
+        FILE *pipe = popen("xrandr", "r");
+        if (!pipe) {
+            fprintf(stderr, "Error executing command.\n");
+            return NULL;
+        }
+
+        int ures = 1;
+
+        while (fgets(buffer, BUFFER_SIZE, pipe)) {
+            if (strstr(buffer, sres)) {
+                char *rate = strtok(buffer, " ");
+                while (rate != NULL && count < BUFFER_SIZE) {  // check if count is less than BUFFER_SIZE before accessing rates array
+                    // remove "+" and newlines
+                    rate[strcspn(rate, "+\n")] = 0;
+
+                    if (!ures && strcmp(rate, "") != 0) {
+                        rates[count] = malloc(strlen(rate) + 1);
+                        strcpy(rates[count], rate);
+
+                        count++;
+                    }
+
+                    if (ures) {
+                        ures = 0;
+                    }
+
+                    rate = strtok(NULL, " ");
+                }
+            }
+        }
+
+        pclose(pipe);
+
+        // Add NULL terminator to array
+        if (count < BUFFER_SIZE) {  // check if count is less than BUFFER_SIZE before accessing rates array
+            rates[count] = NULL;
+        } else {
+            fprintf(stderr, "Array index out of bounds.\n");
+        }
+
+        return rates;
+    }
+
+    char **rates = get_rates();
+
+            g_print("Refresh Rates changed to display mode: %s\n", sres);
+            
+    rates = get_rates();
+    GtkComboBox *refcombo = GTK_COMBO_BOX(user_data);
+    gtk_combo_box_text_remove_all(GTK_COMBO_BOX_TEXT(refcombo));
+    for (int i = 0; rates[i] != NULL; i++) 
+    {
+        printf("%s\n", rates[i]);
+        gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(refcombo), rates[i]);
+        gtk_combo_box_set_active(GTK_COMBO_BOX(refcombo), 0);
+
+    }
+    free(rates);
+
+    g_free(sres);
+
+}
+
+ int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
 
@@ -22,29 +96,24 @@ int main(int argc, char *argv[])
     gtk_container_add(GTK_CONTAINER(window), grid);
 
     /// Widgets
-    GtkWidget *reslabel = gtk_label_new("Resolution:");
     GtkWidget *rescombo = gtk_combo_box_text_new();
 
-    
-    GtkWidget *reflabel = gtk_label_new("Refresh Rate:");
     GtkWidget *refcombo = gtk_combo_box_text_new();
     
-    GtkWidget *rotlabel = gtk_label_new("Rotation:");
     GtkWidget *rotcombo = gtk_combo_box_text_new();
         gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(rotcombo), "normal");
         gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(rotcombo), "left");
         gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(rotcombo), "right");
         gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(rotcombo), "inverted");
     
-    GtkWidget *outlabel = gtk_label_new("Output:");
     GtkWidget *outcombo = gtk_combo_box_text_new();
     GtkWidget *offon = gtk_combo_box_text_new();
         gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(offon), "On");
         gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(offon), "Off");
 
-    GtkWidget *label2 = gtk_label_new("Slider:");
-    GtkWidget *slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 100, 1);
-    gtk_scale_set_draw_value(GTK_SCALE(slider), FALSE);
+    GtkWidget *slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 35, 200, 100);
+    gtk_scale_set_draw_value(GTK_SCALE(slider), TRUE);
+    gtk_widget_set_size_request(slider, 200, 50);
     
     GtkWidget *ctmbtn = gtk_button_new_with_label("Custom Resolution");
     GtkWidget *defbtn    = gtk_button_new_with_label("Default");
@@ -93,22 +162,24 @@ int main(int argc, char *argv[])
     gtk_combo_box_set_active(GTK_COMBO_BOX(offon), 0);
 
     //Items Grid position
-    gtk_grid_attach(GTK_GRID(grid), reslabel, 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Resolution:"), 0, 0, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), rescombo, 1, 0, 1, 1);
     
-    gtk_grid_attach(GTK_GRID(grid), reflabel, 0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Refresh Rate:"), 0, 1, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), refcombo, 1, 1, 1, 1);
     
-    gtk_grid_attach(GTK_GRID(grid), rotlabel, 0, 2, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Rotation:"), 0, 2, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), rotcombo, 1, 2, 1, 1);
     
-    gtk_grid_attach(GTK_GRID(grid), outlabel, 0, 3, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Output:"), 0, 3, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), outcombo, 1, 3, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), offon,    2, 3, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Scale:"), 0, 4, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), slider,   1, 4, 2, 1);
     
-    gtk_grid_attach(GTK_GRID(grid), ctmbtn,   0, 4, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), defbtn,   1, 4, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), applybtn, 2, 4, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), ctmbtn,   0, 5, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), defbtn,   1, 5, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), applybtn, 2, 5, 1, 1);
 
     //command
     void chrs() 
@@ -117,9 +188,14 @@ int main(int argc, char *argv[])
         const char *resolution = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(rescombo));
         const char *refresh_rate = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(refcombo));
         const char *rotation = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(rotcombo));
+        
+        int sld = gtk_range_get_value(GTK_RANGE(slider));
+        double scl = (double)(sld) / (double)(100);
+        char slider[16];
+        snprintf(slider, sizeof(slider), "%.2f%", scl);
     
     char *command = (char*) malloc(sizeof(char) * 100);
-    sprintf(command, "xrandr --output %s --mode %s --rate %s --rotation %s", output, resolution, refresh_rate, rotation);
+    sprintf(command, "xrandr --output %s --mode %s --rate %s --rotation %s --scale %s", output, resolution, refresh_rate, rotation, slider);
     
     system(command);
     free(command);
@@ -144,9 +220,9 @@ int main(int argc, char *argv[])
     void on_apply_button_clicked(GtkButton *button, gpointer user_data) {
     chrs();
     }
-
     g_signal_connect(applybtn, "clicked", G_CALLBACK(on_apply_button_clicked), NULL);
     g_signal_connect(defbtn, "clicked", G_CALLBACK(on_default_button_clicked), NULL);
+    g_signal_connect(rescombo, "changed", G_CALLBACK(on_rescombo_changed), refcombo);
 
     // End
     gtk_widget_show_all(window);
