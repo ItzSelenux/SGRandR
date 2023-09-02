744f1d31213c3a609a528b4260f6634ef788d40f
ItzSelenux <ItzSelenux@zariep.com>
2023-09-01 23:54:28 -0600

    .

--- sgrandr.c
+++ sgrandr.c
@@ -2,7 +2,7 @@
 
  int main(int argc, char *argv[])
 {
-	if (argc > 0) 
+	if (argc > 0)
 	{
 		pm = argv[0];
 	}
@@ -13,7 +13,7 @@
 
 	int testmode = 0;
 
-for(int i = 1; i < argc; i++) 
+for(int i = 1; i < argc; i++)
 {
 	if(strcmp(argv[i], "--nocsd") == 0)
 	{
@@ -22,16 +22,16 @@ for(int i = 1; i < argc; i++)
 	}
 }
 
-for(int i = 1; i < argc; i++) 
+for(int i = 1; i < argc; i++)
 {
-	if(strcmp(argv[i], "--testmode") == 0) 
+	if(strcmp(argv[i], "--testmode") == 0)
 	{
 	  testmode = 1;
 	}
 }
 	gtk_init(&argc, &argv);
 
-if(testmode) 
+if(testmode)
 {
 	printf("--testmode is eneable, displaying all options \n");
 }
@@ -43,7 +43,7 @@ if(testmode)
 	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 	gtk_widget_set_size_request(window, 400, 300);
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	
+
 			theme = gtk_icon_theme_get_default();
 	info = gtk_icon_theme_lookup_icon(theme, "video-display", 48, 0);
 	if (info != NULL)
@@ -56,11 +56,11 @@ if(testmode)
 	//accelerator header
 	accel_group = gtk_accel_group_new();
 		gtk_window_add_accel_group(GTK_WINDOW(window), accel_group);
-	
+
 	// Create the header bar
 	headerbar = gtk_header_bar_new();
 	gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(headerbar), TRUE);
-	
+
 	button = gtk_menu_button_new();
 	image = gtk_image_new_from_icon_name("video-display", GTK_ICON_SIZE_BUTTON);
 	gtk_container_add(GTK_CONTAINER(button), image);
@@ -73,7 +73,7 @@ if(testmode)
 	submenu = gtk_menu_new();
 
 	// Create the submenu items
-	
+
 	submenu_item1 = gtk_menu_item_new_with_label("Add a custom resolution");
 	submenu_item2 = gtk_check_menu_item_new_with_label("Show \"Scaling mode \" option");
 	submenu_item4 = gtk_menu_item_new_with_label("Reload Program");
@@ -99,8 +99,8 @@ if(testmode)
 	{
 	gtk_window_set_titlebar(GTK_WINDOW(window), headerbar);
 	}
-	
-	
+
+
 	// Create grid
 	grid = gtk_grid_new();
 	gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
@@ -116,12 +116,12 @@ if(testmode)
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(rotcombo), "left");
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(rotcombo), "right");
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(rotcombo), "inverted");
-	
+
 	outcombo = gtk_combo_box_text_new();
 	offon = gtk_combo_box_text_new();
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(offon), "On");
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(offon), "Off");
-		
+
 	pos = gtk_combo_box_text_new();
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(pos), "Same as");
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(pos), "Left of");
@@ -134,8 +134,8 @@ if(testmode)
 	gtk_scale_set_draw_value(GTK_SCALE(slider), TRUE);
 	gtk_widget_set_size_request(slider, 200, 50);
 	gtk_range_set_value(GTK_RANGE(slider), 100);
-	
-	
+
+
 	scacombo = gtk_combo_box_text_new();
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(scacombo), "Full");
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(scacombo), "Center");
@@ -144,46 +144,46 @@ if(testmode)
 	scalabel = gtk_label_new("Scaling Mode:");
 	outlabel = gtk_label_new("Output:");
 	poslabel = gtk_label_new("Position:");
-		
+
 	defbtn    = gtk_button_new_with_label("Default");
 		gtk_widget_set_tooltip_text(defbtn, "Ctrl+D");
 	applybtn  = gtk_button_new_with_label("Apply");
 
 	// Get the Values of comboboxes
 	resolutions = get_resolutions();
-	for (int i = 0; resolutions[i] != NULL; i++) 
+	for (int i = 0; resolutions[i] != NULL; i++)
 	{
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(rescombo), resolutions[i]);
 		free(resolutions[i]);
 	}
 	free(resolutions);
-	
+
 	rates = get_rates();
-	for (int i = 0; rates[i] != NULL; i++) 
+	for (int i = 0; rates[i] != NULL; i++)
 	{
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(refcombo), rates[i]);
 		free(rates[i]);
 	}
 	free(rates);
-	
+
 	outputs = get_outputs();
-	for (int i = 0; outputs[i] != NULL; i++) 
+	for (int i = 0; outputs[i] != NULL; i++)
 	{
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(outcombo), outputs[i]);
 		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(outcombo2), outputs[i]);
 		free(outputs[i]);
 	}
 	free(outputs);
-	
+
 	// Open Extra option is there is more than one option
 	model = gtk_combo_box_get_model(GTK_COMBO_BOX(outcombo));
 	int num_rows = gtk_tree_model_iter_n_children(model, NULL);
-	if (!num_rows == 0 || testmode == 1) 
+	if (!num_rows == 1 || testmode == 1)
 	{
 		gtk_grid_attach(GTK_GRID(grid), applybtn, 2, 8, 1, 1);
 		gtk_grid_attach(GTK_GRID(grid), defbtn,   1, 8, 1, 1);
 	}
-	
+
 
 
 	//Set first option of comboboxes as default
@@ -199,16 +199,16 @@ if(testmode)
 	//Items Grid position
 	gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Resolution:"), 0, 0, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), rescombo, 1, 0, 1, 1);
-	
+
 	gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Refresh Rate:"), 0, 1, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), refcombo, 1, 1, 1, 1);
-	
+
 	gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Rotation:"), 0, 2, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), rotcombo, 1, 2, 1, 1);
-	
+
 	gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Scale (%):"), 0, 4, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), slider,   1, 4, 1, 1);
-	
+
 	gtk_grid_attach(GTK_GRID(grid), poslabel, 0, 5, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), pos, 1, 5, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), outcombo2, 2, 5, 1, 1);
@@ -217,10 +217,10 @@ if(testmode)
 	gtk_grid_attach(GTK_GRID(grid), offon, 2, 3, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), scalabel, 0, 7, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), scacombo,   1, 7, 1, 1);
-	
+
 	gtk_grid_attach(GTK_GRID(grid), defbtn,   0, 8, 1, 1);
 	gtk_grid_attach(GTK_GRID(grid), applybtn, 1, 8, 1, 1);
-	
+
 
 	g_signal_connect(applybtn, "clicked", G_CALLBACK(on_apply_button_clicked), &num_rows);
 	g_signal_connect(defbtn, "clicked", G_CALLBACK(on_default_button_clicked), NULL);
@@ -243,23 +243,23 @@ if(testmode)
 
 	// End
 	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-		if (access("/usr/bin/sgrandr-cr", F_OK) == 0) 
+		if (access("/usr/bin/sgrandr-cr", F_OK) == 0)
 	{
 		printf("sgrandr-cr detected \n");
 	}
 	else if
-	(access("./sgrandr-cr", F_OK) == 0) 
+	(access("./sgrandr-cr", F_OK) == 0)
 	{
 		printf("sgrandr-cr detected \n");
-	} 
+	}
 	else
 	{
 		printf("\033[1;33mWARNING\033[0m: sgrandr-cr not detected, hiding button.\n");
 		gtk_widget_hide(submenu_item1);
 	}
-	
+
 		gtk_widget_show_all(window);
-	if (!num_rows == 0 || testmode == 1) 
+	if (!num_rows == 1 || testmode == 1)
 	{
 		gtk_widget_show(pos);
 		gtk_widget_show(offon);
